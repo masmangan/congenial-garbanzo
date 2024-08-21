c6fdc4a647afeafbdfd8d932c9022e031d3b67d9
Marco <marco.mangan@gmail.com>
2024-08-21 12:55:37 +0000

    This change would fix indentation.

--- day05/bee1047/bee1047.cpp
+++ day05/bee1047/bee1047.cpp
@@ -8,18 +8,17 @@ int main()
 
     cin >> h1 >> m1 >> h2 >> m2;
 
-	int t1 = h1 * 60 + m1;
-	int t2 = h2 * 60 + m2;
+    int t1 = h1 * 60 + m1;
+    int t2 = h2 * 60 + m2;
 
-	if (t1 >= t2)
-		t2 += 24 * 60;
+    if (t1 >= t2)
+        t2 += 24 * 60;
 
     int xxx = (t2 - t1) / 60;
     int yyy = (t2 - t1) % 60;
 
-	cout << "O JOGO DUROU " << xxx
-        << " HORA(S) E " << yyy << " MINUTO(S)" << endl;
+    cout << "O JOGO DUROU " << xxx
+         << " HORA(S) E " << yyy << " MINUTO(S)" << endl;
 
     return 0;
 }
-
